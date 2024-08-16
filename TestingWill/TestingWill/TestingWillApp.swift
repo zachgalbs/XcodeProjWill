61938d934b10af5a7f27b9384dc9edf86ff86ae9
zachgalbs <zacharythomasgalbraith@gmail.com>
2024-08-16 13:06:10 -0700

    brother

---
+++ TestingWill/TestingWill/TestingWillApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingWillApp.swift
+//  TestingWill
+//
+//  Created by Zachary Galbraith on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingWillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestingWill/TestingWillTests/TestingWillTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingWillTests.swift
+//  TestingWillTests
+//
+//  Created by Zachary Galbraith on 8/16/24.
+//
+
+import Testing
+@testable import TestingWill
+
+struct TestingWillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
