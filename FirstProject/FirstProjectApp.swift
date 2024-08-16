f25cd08c683172c5a58914d8c8068e4a096942c0
jowint <josephwilliamgalbraith@gmail.com>
2024-08-16 13:10:02 -0700

    init commmit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by William Galbraith on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
